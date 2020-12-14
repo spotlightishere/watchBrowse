bceaccab10922af4913f863f3a572dd1a2a59a1b
Spotlight <spotlight@joscomputing.space>
2020-12-14 13:15:30 -0600

    Use onCommit instead of post-button methodology

--- watchBrowse WatchKit Extension/ContentView.swift
+++ watchBrowse WatchKit Extension/ContentView.swift
@@ -13,27 +13,25 @@ struct ContentView: View {
     
     var body: some View {
         List {
-            TextField("URL", text: $enteredUrl)
-                .textContentType(.URL)
+            TextField("URL", text: $enteredUrl, onCommit: {
+                if !enteredUrl.hasPrefix("http") {
+                    // Let's hope this stays secure...
+                    enteredUrl = "https://" + enteredUrl
+                }
+            })
+            .textContentType(.URL)
+            
             Button("Go to", action: {
-                enteredUrl = goToUrl(givenUrl: enteredUrl)
+                goToUrl(givenUrl: enteredUrl)
             })
             .disabled(enteredUrl == "")
         }
     }
 }
 
-func goToUrl(givenUrl: String) -> String {
-    var url = givenUrl
-    if !url.hasPrefix("http") {
-        // I hope you are secure.
-        url = "https://" + url
-    }
-    
-    let view = Dynamic.SFSafariViewController.initWithURL(URL(string: url))
+func goToUrl(givenUrl: String) {
+    let view = Dynamic.SFSafariViewController.initWithURL(URL(string: givenUrl))
     Dynamic.UIApplication.sharedApplication.keyWindow.rootViewController.presentViewController(view, animated:true, completion: nil)
-    
-    return url
 }
 
 struct ContentView_Previews: PreviewProvider {
